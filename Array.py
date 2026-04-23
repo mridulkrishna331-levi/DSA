e197f0d86ae91c7cba2f21ba73a8c188f861e076
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-04-23 20:14:09 +0530

    Array.py

--- Array.py
+++ Array.py
@@ -63,8 +63,84 @@ from array import *
 # for i in range(0, n):
 #     arr.append(int(input("enter next number")))
 
-# ARRAY M KOI ELEMNT KO SERCH KARNA H
-arr = array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 
-for x in arr:
-    print(x, end="   ")
+# ARRAY M KOI ELEMNT KO SERCH KARNA H
+# arr = array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+# toh uske liye  koi variiable bna lenge let say i name se usme apne array ka name (arr.index) or jis bhi element ko hume find karna h vo element likh denge
+# i = arr.index(7)
+# print(i)
+# for x in arr:
+#     print(x, end="   ")
+
+
+# NUMPY MODULE
+# NUMPY MODULE
+# NUMPY MODULE
+# NUMPY MODULE
+# NUMPY MODULE
+# NUMPY MODULE
+# NUMPY MODULE
+
+# import numpy likhnenge toh hume baar baar numpy.array wagerah likhna hoga toh us chiz ko hatane ke liye hume whi doosra wala tarika aapnana hoga from numpy karke ya fir uska alias bna kar kar sakte h toh aise na kare guru
+
+from numpy import *
+# yha par typecode bhi batane ki zarurat nhi h
+# val = array([1, 2, 3, 4, 5.9])
+# for x in val:
+#     print(x, end="   ")
+
+# numpy se hum hetrogenous array bhi bna sakte h
+
+# hum type code bhi daal sakte h or phir vo usko usi ke hisab se set kar dega
+# val = array([1, 2, 3, 4, 5],float)
+# for x in val:
+#     print(x, end="   ")
+
+# hum ek array banayenge jisme sare element Arthematic Progression m ho vo hum bna sakte h directly with the help of line space
+# val = linspace(10, 20, 5)
+# ISME LAST WALA NUMBER INCLUDED RAHEGA
+# y isme hum teen parameter denge jisme plhe wale m hum batayenge ki number kha se shuru ho rha h and doosre wale m batayenge ki number kha par khatam ho rha h and teesre wale m batayenge ki number ke hume kitne parts karne h
+
+
+# dooosa tarika h jo ki h arrange ka
+# val = arange(10,20,3)
+# isme bhi jo hum teeen parameter denge usme plhe hum denge ki number ko start kh se karna h or doosre wale m batayenge ki end kha par karna h or teesre wale m batayenge ki humko kitna common diffrence chahie
+# OR ISME JO BHI LAST NUMBER RAHEGA VO EXCLUDED RAHEGA
+
+
+# EK HUM BNA SAKTE H 1-D ARAY JISME HUME SARE ELEMENT ZEROES M HO
+# val = zeros(10)
+# or bracket ke ander wali chiz batayegi ki kitne size ka hume banana h
+
+
+# agar aa jaye ki hume 0 or 1 ke alawa kisi or elemnet ka array banana h toh uske liye hum use karenge "full" name ka kuch toh
+# or iske ander do parameter pass karenge plha hum batayneg ki kitne size ka hume aray banana h or doosrew wale m bta denge ki uske ander kya value fill karni h
+
+# val = full(10,5)
+# for x in val:
+#     print(x, end=",")
+
+
+# array ko revverse bhi kar sakte h .reverse name ke function se
+
+
+# MULTI DIMENSIONAL ARRAY
+# means numpy me hum multiimensional array kaise bna sakte h
+
+# Zero dimensional array
+# zero = array(10)
+
+# One dimensional array
+# isme bus ek extra square bracket lagayenge or jtne bhi element dakne daa sakte h or seedha print kara denge
+# one = array([1, 2, 3, 4, 5])
+# print(one)
+
+# Two dimensional array
+# isko bananae ke liye hum sabse plhe squar bracket ko lagayenhge
+# or two dimensional array ka matlab hota h ki collection of one dimensional array
+# two = array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+# print(two)
+
+# Three dimensional array
+three = array([[[1, 2], [3, 4]],  [[5, 6], [7, 8]]])
+print(three)
