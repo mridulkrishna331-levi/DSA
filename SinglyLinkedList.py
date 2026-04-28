75cc96e8f10f37e851a980cd5a759799105b96ae
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-04-29 01:44:08 +0530

    SinglyLinkedList.py

---
+++ SinglyLinkedList.py
@@ -0,0 +1,12 @@
+# jab array ke ander memory ka allocation hota h vo contigous hota h
+# LinkedList hume ek option provide kar sakta h kya ki hum runtime p memory allocation kar sakte h
+# agar program chal rha hota h toh uss time if we need memory to store data so we can add in memory in runtime
+
+
+# linkedList = hum normally ek block allocate karate h jisko NODE bola jata h
+# then node ke do part hote h
+# 1.isme information store hoti h  [ zaruri nhi ki value store karo ek objject bhi store kar sakte h jisme ki mainly sari information hoti h ]
+# 2.isme hota h next wali memory ka address
+
+# Pointer = it is a variable which points to a position / an address
+# jaise blaock ke ander do partition h toh hum likhenge ki jaise plhe wale box ko humne Head bol diya jo ki define kar rha h ki humne plhe wale dubbe ki location ki toh hum likhenege ki Head.info  ya fir Head.next
