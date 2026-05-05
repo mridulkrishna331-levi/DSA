aa1585b16b28c0db1f4a93bc7279f24d22b6938e
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-05-05 19:19:51 +0530

    SinglyLinkedList.py

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -53,46 +53,46 @@
 
 # INSERTION AT THE BEGINNING
 
-# class Node:
-#     def __init__(self, info, next=None):
-#         self.data = info
-#         self.next = next
+class Node:
+    def __init__(self, info, next=None):
+        self.data = info
+        self.next = next
 
 
-# class SinglyLinkedList:
-#     def __init__(self, head=None):
-#         self.head = head
+class SinglyLinkedList:
+    def __init__(self, head=None):
+        self.head = head
 
-#     def InsertAtEnd(self, value):
-#         temp = Node(value)
-#         if (self.head != None):
-#             t1 = self.head
-#             while (t1.next != None):
-#                 t1 = t1.next
-#             t1.next = temp
-#         else:
-#             self.head = temp
+    def InsertAtEnd(self, value):
+        temp = Node(value)
+        if (self.head != None):
+            t1 = self.head
+            while (t1.next != None):
+                t1 = t1.next
+            t1.next = temp
+        else:
+            self.head = temp
 
-#     def InsertAtBeg(self, value):
-#         temp = Node(value)
-#         temp.next = self.head
-#         self.head = temp
-#         self.head = temp
+    def InsertAtBeg(self, value):
+        temp = Node(value)
+        temp.next = self.head
+        self.head = temp
+        self.head = temp
 
-#     def printLL(self):
-#         t1 = self.head
-#         while (t1.next != None):
-#             print(t1.data)
-#             t1 = t1.next
-#         print(t1.data)
+    def printLL(self):
+        t1 = self.head
+        while (t1.next != None):
+            print(t1.data)
+            t1 = t1.next
+        print(t1.data)
 
 
-# obj = SinglyLinkedList()
-# obj.InsertAtEnd(10)
-# obj.InsertAtEnd(20)
-# obj.InsertAtEnd(30)
-# obj.InsertAtBeg(5)
-# obj.printLL()
+obj = SinglyLinkedList()
+obj.InsertAtEnd(10)
+obj.InsertAtEnd(20)
+obj.InsertAtEnd(30)
+obj.InsertAtBeg(5)
+obj.printLL()
 
 
 # INSERTION IN THE MIDDLE AND TO SERCH AN ELEMENT 
