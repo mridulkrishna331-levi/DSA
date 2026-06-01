b41bbf4dd44bc942b6c7f006245dd1b950f6e59a
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-01 22:39:52 +0530

    Prerequisites.py

--- Prerequisites.py
+++ Prerequisites.py
@@ -1,9 +1,22 @@
-print("Hello World")
-monday = 87
-tuesday = 34
-avg = (monday+tuesday)/2
-print(avg)
-name12_ = "Mridul"
+# print("Hello World")
+# monday = 87
+# tuesday = 34
+# avg = (monday+tuesday)/2
+# print(avg)
+# name12_ = "Mridul"
 
-first_name = input()
-print(first_name)
+# first_name = input()
+# print(first_name)
+
+
+# age = int(input())
+# if (age >= 18):
+#     print("eligible")
+# else:
+#     print("Not Eligible")
+
+i = 0
+while i <= 5:
+
+    print(i, end=" ")
+    i += 1
