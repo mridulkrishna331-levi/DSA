ddf4d764c172974a38dc118969ba4b11b6e94afe
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-02 20:45:14 +0530

    recursion.py

--- recursion.py
+++ recursion.py
@@ -1,5 +1,29 @@
-n = 5
-i = 1
-while i <= n:
+# PRINT NUMBERS FROM 1 TO N
+
+# Using loop
+# n = 5
+# i = 1
+# while i <= n:
+#     print(i, end=" ")
+#     i += 1
+
+
+# using recursion
+def printNumbers(i, n):
+    # base case
+    if i > n:
+        return
+    # recursive case
     print(i, end=" ")
-    i += 1
+    # uske baad i ki value ko badhana h and same chiz fir se karni h
+    printNumbers(i+1, n)
+
+
+# FACTORIAL KA CODE BHI AISE HI KARNA H
+def factorial(n):
+    if n == 0:
+        return 1
+    return n*(n-1)
+
+
+print(factorial(7))
