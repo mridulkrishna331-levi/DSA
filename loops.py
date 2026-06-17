4de5952328d715a2f52d2b78e8f5837535f7e1e3
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-17 13:01:31 +0530

    loops.py

--- loops.py
+++ loops.py
@@ -1,17 +1,35 @@
-# FOR LOOP
+# # FOR LOOP
 
-for i in range(5):
+# for i in range(5):
+#     print(i)
+# # range 0 se start hogi and then aakhri tak chalegi jaise yha 5 h toh 4 tak means total 5 honge 0,1,2,3,4 bas 5 print ni hoga
+
+# for o in range(1, 9):
+#     print(o)
+# # yha par jo range h vo 1 se tart hogi and 6 tak chalegi means 5 tak number print honge
+
+# for j in range(1, 9, 2):
+#     print(j)
+# # yha jaise chalga vse hi chalega par 2 step ki deri se means 2 ke gap se chalega
+
+# for k in range(10, 0, -1):
+#     print(k)
+# # yha jaise ki loop reverse chalega then 10 se chalke 0 par chalega andd -1 measn 1-1 step kam hota jayega
+
+
+# WHILE LOOP
+# jab tak condition true h tab tak loop chalega
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+
+# baki BREAK use hota h loop ko turat break karne k liye
+for i in range(10):
+    # print(i) condition bahar hi likhni hoti h ander nhi loop ke
+
+    if i == 7:
+        break
     print(i)
-# range 0 se start hogi and then aakhri tak chalegi jaise yha 5 h toh 4 tak means total 5 honge 0,1,2,3,4 bas 5 print ni hoga
-
-for o in range(1, 9):
-    print(o)
-# yha par jo range h vo 1 se tart hogi and 6 tak chalegi means 5 tak number print honge
-
-for j in range(1, 9, 2):
-    print(j)
-# yha jaise chalga vse hi chalega par 2 step ki deri se means 2 ke gap se chalega
-
-for k in range(10, 0, -1):
-    print(k)
-# yha jaise ki loop reverse chalega then 10 se chalke 0 par chalega andd -1 measn 1-1 step kam hota jayega
